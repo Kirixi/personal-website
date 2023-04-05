45ae263a395aee12eef97ae249d4e486b55b816f
Jonathan <tehyeuhaw@gmail.com>
2023-04-05 21:40:26 +1000

    feat: work experience section completed

--- src/assets/index.js
+++ src/assets/index.js
@@ -17,10 +17,7 @@ import tailwind from './tech/tailwind.png';
 import typescript from './tech/typescript.png';
 import threejs from './tech/threejs.svg';
 
-import meta from './company/meta.png';
-import shopify from './company/shopify.png';
-import starbucks from './company/starbucks.png';
-import tesla from './company/tesla.png';
+import personalbanker from './company/PersonalBanker.png';
 
 import carrent from './carrent.png';
 import jobit from './jobit.png';
@@ -48,10 +45,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
@@ -59,4 +52,5 @@ export {
   mandarin,
   english,
   cantonese,
+  personalbanker,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -11,10 +11,6 @@ import {
   git,
   figma,
   docker,
-  meta,
-  starbucks,
-  tesla,
-  shopify,
   carrent,
   jobit,
   tripguide,
@@ -23,6 +19,7 @@ import {
   cantonese,
   mandarin,
   english,
+  personalbanker,
 } from '../assets';
 
 const navLinks = [
@@ -120,55 +117,17 @@ const technologies = [
 
 const experiences = [
   {
-    title: 'React.js Developer',
-    company_name: 'Starbucks',
-    icon: starbucks,
-    iconBg: '#383E56',
-    date: 'March 2020 - April 2021',
+    title: 'Chatbot Developer',
+    company_name: 'PersonalBanker',
+    icon: personalbanker,
+    iconBg: '#ffffff',
+    date: 'March 2022 - July 2022',
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
-    title: 'React Native Developer',
-    company_name: 'Tesla',
-    icon: tesla,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2021 - Feb 2022',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
-    title: 'Web Developer',
-    company_name: 'Shopify',
-    icon: shopify,
-    iconBg: '#383E56',
-    date: 'Jan 2022 - Jan 2023',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
-    ],
-  },
-  {
-    title: 'Full stack Developer',
-    company_name: 'Meta',
-    icon: meta,
-    iconBg: '#E6DEDD',
-    date: 'Jan 2023 - Present',
-    points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      'Frontend chatbot development with Tawk using Agile based methodology coordinated with cross-country teams',
+      'Tasked with Designing and improving an exisiting chatbot for customer assistance.',
+      'Developed a login and signup prototype, allowing for seamless testing and quick iterations.',
+      'Developed a proxy server that effectively managed requests and responses from and to the backend server.',
+      'Collaborated with tech-lead and product managers to iterate additional chatbot functionalities.',
     ],
   },
 ];
