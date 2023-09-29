cebb5e64886fdf537f91346073622da64b015e04
Jonathan <yeuhaw@fomatictech.com>
2023-09-29 10:57:23 +0800

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915eff]'>Jonathan</span>
+            Hi, I'm <span className='text-[#915eff]'>Jonathan Tay</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white`}>
             I'm a computer science major fresh graduate from{' '}
