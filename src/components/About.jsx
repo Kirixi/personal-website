7b1be008186aa057192418efb88c554c973ac945
Jonathan <tehyeuhaw@gmail.com>
2023-10-31 15:16:41 +0800

    update: updated about paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import Tilt from 'react-parallax-tilt';
-import { motion } from 'framer-motion';
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
 
-import { styles } from '../style';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon, proficiency }) => {
   return (
-    <div className='xs:w-[250px] w-full'>
+    <div className="xs:w-[250px] w-full">
       <motion.div
-        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
           options={{ max: 45, scale: 1, speed: 450 }}
-          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
-          <h3 className='text-white text-[20px] font-bold text-center'>
-            {' '}
-            {title}
-          </h3>
-          <h5 className='text-white text-[15px] text-center'> {proficiency}</h5>
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center"> {title}</h3>
+          <h5 className="text-white text-[15px] text-center"> {proficiency}</h5>
         </div>
       </motion.div>
     </div>
@@ -37,19 +34,13 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-        I am a recent Computer Science graduate seeking a starting position as a
-        software developer. With a strong background in programming and a keen
-        interest in cloud infrastructure and front-end development, I am a quick
-        learner and I am eager to contribute new ideas and strategies in my new
-        position. In addition, I have a strong interest in Korean and Japanese
-        culture and I can speak the following languages listed below.
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        I am currently a Junior Frontend Engineer at a software solutions company. With a new found interest in cloud infrastructure, I
+        would like to transition to cloud or Devlops engineer position in the near future. In addition, I have a keen interest in Korean and
+        Japanese culture and I can speak the following languages listed below.
       </motion.p>
 
-      <div className='mt-20 flex flex-wrap gap-10'>
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
@@ -58,4 +49,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, "about");
